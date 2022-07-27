55a4bb50f7540d7d294aaf3f385bbd0a87f4bb8c
Mustafa <57066099+mohdmustafa0111@users.noreply.github.com>
2022-07-27 22:39:55 +0530

    changed the code completely

--- script.js
+++ script.js
@@ -1,90 +1,65 @@
-/* Select all the necessary Elements  */
-var input = document.querySelector(".todo_input");
-var MainTodoContainer = document.getElementById("todos");
-var addingButton = document.querySelector(".add-item");
-var deleteAllBtn = document.querySelector(".deleteBtn");
-var completedButton = document.querySelector(".completed");
-var removeButton = document.querySelector(".trash");
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
 
-addingButton.addEventListener("click", function (e) {
-  /* stoping button behaviour */
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
+}
+
+form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  /* Create all the elements */
-  if (input.value.trim()) {
-    /* UL Tag */
-    var ulTag = document.createElement("ul");
-    ulTag.classList.add("todo-list-container");
-    /* Todo list div */
-    var todoList = document.createElement("div");
-    todoList.classList.add("todo-list");
-    /* LI Tag */
-    var liTag = document.createElement("li");
-    liTag.innerText = input.value;
-    liTag.classList.add("todo-item");
-    /* Button Div */
-    var buttonDiv = document.createElement("div");
-    buttonDiv.classList.add("button");
-    /* completed button element1 */
-    var completeButton = document.createElement("button");
-    completeButton.classList.add("completed");
-    completeButton.innerHTML = '<i class="fas fa-check"></i>';
-    /* Edit Button */
-    var editBtn = document.createElement("button");
-    editBtn.innerHTML = '<i class="far fa-edit"></i>';
-    editBtn.classList.add("editBtn");
-    editBtn.onclick = function () {
-      editWorking(liTag);
-    };
-    /* trash button element2 */
-    var trashButton = document.createElement("button");
-    trashButton.classList.add("trash");
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-
-    /* Appending Elements into each other */
-    ulTag.appendChild(todoList);
-    todoList.appendChild(liTag);
-    todoList.appendChild(buttonDiv);
-    buttonDiv.appendChild(completeButton);
-    buttonDiv.appendChild(editBtn);
-    buttonDiv.appendChild(trashButton);
-
-    /* if input is empty then don't display empty list in DOM */
-    MainTodoContainer.appendChild(ulTag);
-
-    /* sessionStorage */
-    /* when the add button click clear the input value */
-    input.value = "";
-    /* complete and trash button working */
-    todoList.addEventListener("click", function (e) {
-      var items = e.target;
-      if (items.classList[0] === "completed") {
-        var todo = items.parentElement;
-        var todo2 = todo.parentElement;
-        todo2.classList.add("line_through");
-      } else if (items.classList[0] === "trash") {
-        var todo = items.parentElement;
-        var todo2 = todo.parentElement;
-        todo2.classList.add("fall");
-        todo2.addEventListener("transitionend", function () {
-          var todo3 = todo2.parentElement;
-          todo3.remove();
-        });
-      }
-    });
-  } else if (input.value === "") {
-    alert("please fill the input field");
-  }
+  addTodo();
 });
 
-function editWorking(e) {
-  var editValue = prompt("edit the select item", e.firstChild.nodeValue);
-  e.firstChild.nodeValue = editValue;
-}
-function deleteAllElements() {
-  var gettingUlTag = document.querySelectorAll(".todo-list-container");
-  for (var i = 0; i < gettingUlTag.length; i++) {
-    gettingUlTag[i].remove();
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
   }
-  input.value = "";
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
+
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+
+      todoEl.remove();
+      updateLS();
+    });
+
+    todosUL.appendChild(todoEl);
+
+    input.value = "";
+
+    updateLS();
+  }
+}
+
+function updateLS() {
+  todosEl = document.querySelectorAll("li");
+
+  const todos = [];
+
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
